0de1f7b5437b11e236ec88f1a532353b0057c04b
patryksek <patryk.sek1@wp.pl>
2017-07-16 23:18:13 +0200

    Add files via upload

---
+++ Program/Program/Class1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Calculator:ICalculator
+    {
+        public void Add()
+        {
+            double w = 0;
+            int x = 0, y = 0;
+            string Temp;
+
+            Console.Write("Podaj pierwsza liczbe: ");
+            try
+            {
+                Temp = Console.ReadLine();
+                x = Convert.ToInt32(Temp);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("DESPACITO! Ciepaj ino liczbe");
+            }
+            
+
+            Console.Write("Podaj druga liczbe: ");
+
+            try
+            {
+                Temp = Console.ReadLine();
+                y = Convert.ToInt32(Temp);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("DESPACITO! Ciepaj ino liczbe");
+            }
+           
+            w = x + y;
+
+            Console.WriteLine("Wynik: {0}", w);
+            Console.ReadLine();
+        }
+
+        public void Sub()
+        {
+            double w = 0;
+            int x = 0, y = 0;
+            string Temp;
+
+            Console.Write("Podaj pierwsza liczbe: ");
+            try
+            {
+                Temp = Console.ReadLine();
+                x = Convert.ToInt32(Temp);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("DESPACITO! Ciepaj ino liczbe");
+            }
+
+
+            Console.Write("Podaj druga liczbe: ");
+
+            try
+            {
+                Temp = Console.ReadLine();
+                y = Convert.ToInt32(Temp);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("DESPACITO! Ciepaj ino liczbe");
+            }
+
+            w = x - y;
+
+            Console.WriteLine("Wynik: {0}", w);
+            Console.ReadLine();
+        }
+
+        public void Mul()
+        {
+            double w = 0;
+            int x = 0, y = 0;
+            string Temp;
+
+            Console.Write("Podaj pierwsza liczbe: ");
+            try
+            {
+                Temp = Console.ReadLine();
+                x = Convert.ToInt32(Temp);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("DESPACITO! Ciepaj ino liczbe");
+            }
+
+
+            Console.Write("Podaj druga liczbe: ");
+
+            try
+            {
+                Temp = Console.ReadLine();
+                y = Convert.ToInt32(Temp);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("DESPACITO! Ciepaj ino liczbe");
+            }
+
+            w = x * y;
+
+            Console.WriteLine("Wynik: {0}", w);
+            Console.ReadLine();
+        }
+
+        public void Div()
+        {
+            double w = 0;
+            int x = 0, y = 0;
+            string Temp;
+
+            Console.Write("Podaj pierwsza liczbe: ");
+            try
+            {
+                Temp = Console.ReadLine();
+                x = Convert.ToInt32(Temp);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("DESPACITO! Ciepaj ino liczbe");
+            }
+
+
+            Console.Write("Podaj druga liczbe: ");
+
+            try
+            {
+                Temp = Console.ReadLine();
+                y = Convert.ToInt32(Temp);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("DESPACITO! Ciepaj ino liczbe");
+            }
+
+            try
+            {
+                w = x / y;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Serio Janusz? Dzielic przez 0?");
+                Console.ReadLine();
+            }
+            
+            Console.WriteLine("Wynik: {0}", w);
+            Console.ReadLine();
+        }
+
+        public void IsPrimary()
+        {
+            string Temp;
+            int x = 0;
+            Console.WriteLine("Sprawdz, czy podana liczba jest liczba pierwsza!");
+            Console.Write("Podaj liczbe: ");
+            
+            try
+            {
+                Temp = Console.ReadLine();
+                x = Convert.ToInt32(Temp);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wprowadzono bledna wartosc");
+            }
+
+            if (x<2) Console.WriteLine("Liczba nie jest pierwsza");
+                else
+                {
+                for (int i = 2; i*i < x; i++)
+                {
+                    if (x % i == 0) Console.WriteLine("Liczba nie jest pierwsza");
+                    else Console.WriteLine("Liczba jest liczba pierwsza!");
+                }
+                }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program/Program/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program:Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word! Wybierz, co chcesz zrobic");
+            Console.WriteLine("1: Dodawanie");
+            Console.WriteLine("2: Odejmowanie");
+            Console.WriteLine("3: Mnozenie");
+            Console.WriteLine("4: Dzielenie");
+            Console.WriteLine("5: Sprawdzanie, czy liczba jest piersza");
+            Console.WriteLine("6: format C:/");
+
+            int Check = int.Parse(Console.ReadLine());
+
+          
+            switch (Check)
+            {
+                case 1:
+                    Calculator c1 = new Calculator();
+                    c1.Add();
+                    break;
+
+                case 2:
+                    Calculator c2 = new Calculator();
+                    c2.Sub();
+                    break;
+
+                case 3:
+                    Calculator c3 = new Calculator();
+                    c3.Mul();
+                    break;
+
+                case 4:
+                    Calculator c4 = new Calculator();
+                    c4.Div();
+                    break;
+
+                case 5:
+                    Calculator c5 = new Calculator();
+                    c5.IsPrimary();
+                    break;
+
+                case 6:
+                    Console.WriteLine("Nie tym razem :)");
+                    Console.ReadLine();
+                    break;
+                    
+            }
+        }
+    }
+
+}
